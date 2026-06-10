1fe09a6940a49625a411526b403c2a74243be98b
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-10 19:58:44 -0300

    refactor: adiciona busca de veiculo por clienteId

--- src/repositories/veiculo/drizzle-veiculo-repository.ts
+++ src/repositories/veiculo/drizzle-veiculo-repository.ts
@@ -4,43 +4,47 @@ import { db } from "@/src/db/drizzle";
 import { veiculos } from "@/src/db/drizzle/schema";
 import { eq } from "drizzle-orm";
 
-export class DrizzleVeiculoRepository implements VeiculoRepository{
-    
+export class DrizzleVeiculoRepository implements VeiculoRepository {
+
     async listarVeiculos(): Promise<VeiculoModel[]> {
         return await db.select().from(veiculos);
     }
 
     async buscarVeiculoPorId(id: number): Promise<VeiculoModel | null> {
-        const [veiculo] = await db.select().from(veiculos).where(eq(veiculos.id,id))
+        const [veiculo] = await db.select().from(veiculos).where(eq(veiculos.id, id))
         return veiculo ?? null
     }
 
     async buscarVeiculoPorPlaca(placa: string): Promise<VeiculoModel | null> {
-        const [veiculo] = await db.select().from(veiculos).where(eq(veiculos.placa,placa)).limit(1);
-        return veiculo ?? null  
+        const [veiculo] = await db.select().from(veiculos).where(eq(veiculos.placa, placa)).limit(1);
+        return veiculo ?? null
     }
-     
+
+    async buscarVeiculoPorClienteId(clienteId: number): Promise<VeiculoModel[]> {
+        return await db.select().from(veiculos).where(eq(veiculos.clienteId, clienteId))
+    }
+
     async registrarVeiculo(veiculo: CreateVeiculoInput): Promise<VeiculoModel> {
         const result = await db.insert(veiculos).values(veiculo).$returningId();
-        
-        const id = result[0]?.id ;
-        if(!id)throw new Error("Veículo criado, mas o ID não foi retornado.");
+
+        const id = result[0]?.id;
+        if (!id) throw new Error("Veículo criado, mas o ID não foi retornado.");
 
         const veiculoCriado = await this.buscarVeiculoPorId(id);
 
-        if(!veiculoCriado) throw new Error(" Veículo criado, mas não foi possivel buscá-lo");
+        if (!veiculoCriado) throw new Error(" Veículo criado, mas não foi possivel buscá-lo");
 
         return veiculoCriado;
     }
-    
+
     async atualizarVeiculo(id: number, veiculo: UpdateVeiculoInput): Promise<VeiculoModel | null> {
-        await db.update(veiculos).set(veiculo).where(eq(veiculos.id,id));
+        await db.update(veiculos).set(veiculo).where(eq(veiculos.id, id));
         return await this.buscarVeiculoPorId(id);
     }
 
     async deletarVeiculo(id: number): Promise<boolean> {
-        await db.delete(veiculos).where(eq(veiculos.id,id));
+        await db.delete(veiculos).where(eq(veiculos.id, id));
         return true;
-    }  
+    }
 
 }
--- src/repositories/veiculo/veiculo-repository.ts
+++ src/repositories/veiculo/veiculo-repository.ts
@@ -1,13 +1,11 @@
 import { CreateVeiculoInput, UpdateVeiculoInput, VeiculoModel } from "@/src/model/veiculo/veiculo-model";
 
-export interface VeiculoRepository{
-    listarVeiculos():Promise<VeiculoModel[]>;
-    buscarVeiculoPorId(id:number):Promise<VeiculoModel | null>;
-    buscarVeiculoPorPlaca(placa:string):Promise<VeiculoModel|null>
-    registrarVeiculo(veiculo:CreateVeiculoInput):Promise<VeiculoModel>
-    atualizarVeiculo(id:number, veiculo:UpdateVeiculoInput):Promise<VeiculoModel | null>;
+export interface VeiculoRepository {
+    listarVeiculos(): Promise<VeiculoModel[]>;
+    buscarVeiculoPorId(id: number): Promise<VeiculoModel | null>;
+    buscarVeiculoPorPlaca(placa: string): Promise<VeiculoModel | null>;
+    buscarVeiculoPorClienteId(clienteId: number): Promise<VeiculoModel[]>;
+    registrarVeiculo(veiculo: CreateVeiculoInput): Promise<VeiculoModel>;
+    atualizarVeiculo(id: number, veiculo: UpdateVeiculoInput): Promise<VeiculoModel | null>;
     deletarVeiculo(id: number): Promise<boolean>;
 }
-
-    
-    

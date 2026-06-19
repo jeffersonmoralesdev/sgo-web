80e113d4da6cab235e75967f784e2cbdeed9d8ec
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-19 10:59:55 -0300

    refactor:adiciona parametros de usuários, para serem utilizados em toast.

--- src/constants/parametros-toast/index.ts
+++ src/constants/parametros-toast/index.ts
@@ -2,5 +2,7 @@ export const PARAMETROS_TOAST = {
     "sessao-expirada": "Sessão não encontrada ou expirada, por segurança faça login novamente.",
     "cliente-atualizado": "Cliente atualizado com sucesso.",
     "cliente-cadastrado": "Cliente cadastrado com sucesso.",
+    "usuario-atualizado": "Usuário atualizado com sucesso.",
+    "usuario-cadastrado": "Usuário cadastrado com sucesso.",
 }
 export type ParametrosToast = keyof typeof PARAMETROS_TOAST;

474c636c75851e48ffcb407201c4a495917f63ea
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-19 11:09:18 -0300

    refactor:ajusta regra para tornar campo senha opcional em formulario edição.

--- src/validator/usuario/usuario-validator.ts
+++ src/validator/usuario/usuario-validator.ts
@@ -9,9 +9,21 @@ export const createUsuarioSchema = z.object({
         pipe(z.enum(['ADMIN', 'OPERADOR'], "Perfil deve ser 'ADMIN' ou 'OPERADOR'")) 
 });
 
-export const updateUsuarioInputSchema = createUsuarioSchema.partial().extend({
-    ativo: z.boolean().optional()}).
-    refine((data)=> Object.keys(data).length > 0, "Pelo menos um campo deve ser fornecido para atualização");
+const senhaOpcionalUpdateSchema = z.preprocess(
+    (value) => value === "" ? undefined : value,
+    z.string().min(6, "A senha deve ter pelo menos 6 caracteres").optional()
+);
+
+export const updateUsuarioInputSchema = z.object({
+    nome:z.string().min(3, "O nome deve ter pelo menos 3 caracteres").optional(),
+    email: z.email("Email inválido").trim().toLowerCase().optional(),
+    senha:senhaOpcionalUpdateSchema,
+    perfil:z.string().trim().
+        transform((perfil)=> perfil.toUpperCase()).
+        pipe(z.enum(["ADMIN","OPERADOR"], "Perfil deve ser 'ADMIN' ou 'OPERADOR'")).
+        optional(),
+    ativo: z.boolean().optional()
+}).refine((data)=> Object.keys(data).length > 0, "Pelo menos um campo deve ser fornecido para atualização");
 
 export const idUsuarioSchema = z.preprocess((value) => value === "" ? undefined : value,
     z.coerce.number({ error: "ID inválido" }).

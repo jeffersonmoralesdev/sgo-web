00da2cce99736551ff122e29a1326fb1317d7918
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-20 23:09:42 -0300

    refactor:ajuste, para tornar dados normalizados.

--- src/validator/cliente/cliente-validator.ts
+++ src/validator/cliente/cliente-validator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { verificarCpf } from "@/src/utils/validar-cpf";
 
 export const createClienteSchema = z.object({
-    nome: z.string().min(3, "nome deve ter pelo menos 3 caracteres"),
+    nome: z.string().toLowerCase().min(3, "nome deve ter pelo menos 3 caracteres"),
     cpf: z.string()
         .transform((cpf) => cpf.replace(/\D/g, ""))
         .refine((cpf) => cpf.length === 11, "CPF deve conter exatamente 11 números")
@@ -10,7 +10,7 @@ export const createClienteSchema = z.object({
     telefone: z.string()
         .transform((telefone) => telefone.replace(/\D/g, ""))
         .refine((telefone) => telefone.length >= 10 && telefone.length <= 11, "Telefone deve conter entre 10 e 11 números"),
-    email: z.email("Email inválido"),
+    email: z.email("Email inválido").toLowerCase(),
 })
 export const idClienteSchema = z.preprocess((value) => value === "" ? undefined : value,
     z.coerce.number({ error: "ID inválido" }).

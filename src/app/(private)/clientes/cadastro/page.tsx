923fc359632f16f4072d2767baabd7f7e122b8ee
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-20 22:54:12 -0300

    refactor:ajuste para pegar o prametro quando retorno quando vir.

--- src/app/(private)/clientes/cadastro/page.tsx
+++ src/app/(private)/clientes/cadastro/page.tsx
@@ -1,15 +1,23 @@
 
 import { FormularioCliente } from "@/src/components/private/cliente/formulario-cliente";
 import { HeaderPage } from "@/src/components/private/header-page";
+type CadastroClientePageProps = {
+    searchParams: Promise<{
+        retorno?: string;
+    }>;
+}
 
-export default function CadastroClientePage() {
+
+
+export default async function CadastroClientePage({searchParams}:CadastroClientePageProps) {
+    const {retorno} = await searchParams
     return (
         <main className="space-y-6">
             <HeaderPage
                 titulo="Cadastrar cliente"
                 descricao="Preencha as informações necessárias para registrar um novo cliente na oficina."
             />
-            <FormularioCliente modo="cadastro" />
+            <FormularioCliente modo="cadastro" paramsRetorno={retorno} />
         </main>
     )
 }

124f0b3897caef758ce122b7501447736b47ba8e
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-06-21 11:19:45 -0300

    refactor:ajuste para adicionar regra de negocio onde um veiculo não pode ser cadastrado e nem atualizado com cliente inativo.

--- src/services/veiculo/veiculo-service.ts
+++ src/services/veiculo/veiculo-service.ts
@@ -1,67 +1,75 @@
+import { VeiculoError } from "@/src/errors/veiculo-error";
 import { CreateVeiculoInput, UpdateVeiculoInput, VeiculoModel } from "@/src/model/veiculo/veiculo-model";
 import { ClienteRepository } from "@/src/repositories/cliente/cliente-repository";
 import { VeiculoRepository } from "@/src/repositories/veiculo/veiculo-repository";
 
-export class VeiculoService{
+export class VeiculoService {
     constructor(
-        private readonly veiculoRepository:VeiculoRepository,
-        private readonly clienteRepository:ClienteRepository
-    ){}
+        private readonly veiculoRepository: VeiculoRepository,
+        private readonly clienteRepository: ClienteRepository
+    ) { }
 
-    async listarVeiculos():Promise<VeiculoModel[]>{
-        return await this.veiculoRepository.listarVeiculos();
+    async listarVeiculos(busca?: string): Promise<VeiculoModel[]> {
+        return await this.veiculoRepository.listarVeiculos(busca);
     };
 
-    async buscarVeiculoPorId(id:number):Promise<VeiculoModel|null>{
+    async buscarVeiculoPorId(id: number): Promise<VeiculoModel | null> {
         return await this.veiculoRepository.buscarVeiculoPorId(id);
     };
 
-    async buscarVeiculoPorPlaca(placa:string):Promise<VeiculoModel|null>{
+    async buscarVeiculoPorPlaca(placa: string): Promise<VeiculoModel | null> {
         return await this.veiculoRepository.buscarVeiculoPorPlaca(placa);
     };
 
-    async registrarVeiculo(veiculo:CreateVeiculoInput):Promise<VeiculoModel>{
+    async registrarVeiculo(veiculo: CreateVeiculoInput): Promise<VeiculoModel> {
 
         const cliente = await this.clienteRepository.buscarClientePorId(veiculo.clienteId);
-        
-        if(!cliente)throw new Error("Cliente responsavel do veículo não encontrado");
+
+        if (!cliente) throw new VeiculoError("Cliente responsavel do veículo não encontrado");
+        if (cliente.ativo === false) throw new VeiculoError("Não foi possível registrar o veículo, cliente inativo verifique os dados e tente novamente.");
 
         const veiculoValidation = await this.veiculoRepository.buscarVeiculoPorPlaca(veiculo.placa);
-    
-        if(veiculoValidation)throw new Error("Já existe um veículo cadastrado com esta placa.");
-                
+
+        if (veiculoValidation) throw new VeiculoError("Já existe um veículo cadastrado com esta placa.");
+
         const veiculoCadastrado = await this.veiculoRepository.registrarVeiculo(veiculo);
 
-        return veiculoCadastrado  
+        return veiculoCadastrado
     };
 
-    async atualizarVeiculo(id:number,veiculo:UpdateVeiculoInput):Promise<VeiculoModel>{
-        
-    
+    async atualizarVeiculo(id: number, veiculo: UpdateVeiculoInput): Promise<VeiculoModel> {
+
+
         const veiculoExistente = await this.buscarVeiculoPorId(id);
-        if(!veiculoExistente) throw new Error ("Veículo não encontrado.")
-        
-        if(veiculo.placa && veiculo.placa !== veiculoExistente.placa){
+        if (!veiculoExistente) throw new VeiculoError("Veículo não encontrado.")
+
+        if (veiculo.placa && veiculo.placa !== veiculoExistente.placa) {
             const placaDuplicada = await this.buscarVeiculoPorPlaca(veiculo.placa);
-            if(placaDuplicada) throw new Error("Já existe um veículo cadastrado com esta placa.");
+            if (placaDuplicada) throw new VeiculoError("Já existe um veículo cadastrado com esta placa.");
         }
 
-        if(veiculo.clienteId && veiculo.clienteId !== veiculoExistente.clienteId){   
+        if (veiculo.clienteId && veiculo.clienteId !== veiculoExistente.clienteId) {
             const cliente = await this.clienteRepository.buscarClientePorId(veiculo.clienteId);
-            if (!cliente) throw new Error("Não foi possível atualizar veículo, cliente não encontrado."); 
+            if (!cliente) throw new VeiculoError("Não foi possível atualizar veículo, cliente não encontrado.");
+            if (!cliente.ativo) throw new VeiculoError("Não foi possível atualizar veículo, cliente inativo verifique os dados e tente novamente.");
         }
 
-        const veiculoAtualizado = await this.veiculoRepository.atualizarVeiculo(id,veiculo);
-        if(!veiculoAtualizado) throw new Error("Não foi possivel atualizar veículo.");
-        
+        const veiculoAtualizado = await this.veiculoRepository.atualizarVeiculo(id, veiculo);
+        if (!veiculoAtualizado) throw new VeiculoError("Não foi possível atualizar o veículo, tente novamente em instantes.");
+
         return veiculoAtualizado;
     };
 
-    async deletarVeiculo(id:number):Promise<boolean>{
+    async deletarVeiculo(id: number): Promise<boolean> {
         const veiculoExistente = await this.buscarVeiculoPorId(id);
-        if(!veiculoExistente) throw new Error("Veículo não encontrado");
-        
-        return await this.veiculoRepository.deletarVeiculo(id);
+        if (!veiculoExistente) throw new VeiculoError("Veículo não encontrado");
+
+        //PENDENCIA
+        /*const ordemSevicoVinculado = await this.ordemServicoRepository.buscarOrdenServicoPorVeiculoId(veiculoExistente.id);
+        if (ordemSevicoVinculado.length > 0) throw new VeiculoError("Não é possível excluir este veículo, ele esta vinculado a uma ou mais ordens de serviços.");*/
+
+        await this.veiculoRepository.deletarVeiculo(veiculoExistente.id);
+        return true
     }
-    
+
 }

69d8aaf13d871c1a18a170102fbb5d6c2f5b87f8
jeffersonmoralesdev <moralestech.dev@gmail.com>
2026-07-06 10:02:00 -0300

    refactor:ajuste no tipo query-response tornando data obrigatorio quando houver success na operação

--- src/types/query-response.ts
+++ src/types/query-response.ts
@@ -1,9 +1,14 @@
-export type QueryStatus = "SUCCESS" | "ERROR" ;
+export type QueryStatus = "SUCCESS" | "ERROR";
 
-export type QueryResponse<T = unknown> = {
-    success: boolean;
-    data?: T;
+export type QueryResponse<T = unknown> = | {
+    success: true;
+    status: "SUCCESS";
+    data: T;
     message?: string;
-    error?: string;
-    status: QueryStatus;
+} | {
+    success: false;
+    status: "ERROR";
+    error: string;
+    message?: string;
+    data?: undefined;
 };
